a7da5e0a5cfc49c9bc0e8052b3a9fbdfe20a0da2
Ajay-NooB143 <a7010705312@gmail.com>
2026-04-17 11:40:37 +0530

    Add RL agent and smart execution logic

---
+++ rl_agent.py
@@ -0,0 +1,14 @@
+class RLAgent:
+    def decide(self, features):
+        # state: price, orderbook, indicators
+        # action: buy / sell / hold
+        return "BUY"
+
+
+def smart_execute(signal, orderbook):
+    spread = float(orderbook['asks'][0][0]) - float(orderbook['bids'][0][0])
+
+    if spread < 0.5:
+        return "LIMIT ORDER"
+    else:
+        return "WAIT"
